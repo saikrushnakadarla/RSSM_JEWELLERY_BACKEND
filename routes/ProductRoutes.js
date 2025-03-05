b39367fc97761d50544df0c6dad239db43475f57
Saikrushna <kadarlasaikrushna01@gmail.com>
2025-03-05 11:54:11 +0530

    Saikrushna

--- routes/ProductRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/ProductController');
-
-// Routes for products
-router.post('/add-product', productController.addProduct);
-router.get('/products', productController.getProducts);
-
-module.exports = router;
