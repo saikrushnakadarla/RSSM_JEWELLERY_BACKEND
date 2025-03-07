efe67038085bd70bf82ff9b97ae7965e2305384f
Tharun <tharunkumarreddy1212@gmail.com>
2025-03-07 19:03:17 +0530

    Tharun

--- models/vendorModel.js
+++ models/vendorModel.js
@@ -1,29 +1,34 @@
-const db = require('../db');
+// const db = require('../db');
 
-const addVendor = (values, callback) => {
-  const query = `
-    INSERT INTO vendors (
-      vendor_name, mobile, email, address, city, pincode, state, state_code, 
-      bank_account_number, bank_name, ifsc_code, branch, gst_number, pan_card, aadhaar_card,password
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)
-  `;
-  db.query(query, values, callback);
-};
+// const addVendor = (values, callback) => {
+//   const query = `
+//     INSERT INTO vendors (
+//       vendor_name, mobile, email, address, city, pincode, state, state_code,
+//       bank_account_number, bank_name, ifsc_code, branch, gst_number, pan_card, aadhaar_card, password
+//     ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+//   `;
+//   db.query(query, values, callback);
+// };
 
+// const getAllVendors = (callback) => {
+//   const query = "SELECT * FROM vendors ORDER BY id DESC";
+//   db.query(query, (err, results) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+// };
 
+// const updateVendorStatus = (vendorId, status, callback) => {
+//   const query = "UPDATE vendors SET status = ? WHERE id = ?";
+//   db.query(query, [status, vendorId], callback);
+// };
 
+// const getVendorById = (vendorId, callback) => {
+//   const query = "SELECT vendor_name, email, password FROM vendors WHERE id = ?";
+//   db.query(query, [vendorId], callback);
+// };
 
-// Function to fetch all vendors
-const getAllVendors = (callback) => {
-  const query = "SELECT * FROM vendors ORDER BY id DESC";
-  db.query(query, (err, results) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
-};
-
-
-module.exports = { addVendor,getAllVendors};
+// module.exports = { addVendor, getAllVendors, updateVendorStatus, getVendorById };
