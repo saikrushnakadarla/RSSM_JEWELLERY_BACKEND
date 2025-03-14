dcfc3d00548124269d4510fa33bfe7ca8f3d891c
unknown <sharvanikokkonda@gmail.com>
2025-03-14 18:39:47 +0530

    added editProductRoute

--- db.js
+++ db.js
@@ -1,46 +1,31 @@
 const mysql = require("mysql2");
 
 
-const db = mysql.createConnection({
-
-    host: "localhost",
-    user: "root",
-    password: "Manju@123",
-    database: "rssm_db",
-    port: 3306,
-  });
-
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'ksk1005',
-//     database: 'rssm_db',
-//     port: 3306,
-// });
-
 // const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'ksk1005',
-//     database: 'rssm_db',
-//     port: 3306,
-// });
 
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'ksk1005',
-//     database: 'rssm_db',
-//     port: 3306,
-// });
-
-
-// const db = mysql.createConnection({
 //     host: "localhost",
 //     user: "root",
-//     password: "sharvani@123",
+//     password: "Manju@123",
 //     database: "rssm_db",
-//     port: 3307,
+//     port: 3306,
 //   });
+
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'ksk1005',
+//     database: 'rssm_db',
+//     port: 3306,
+// });
+
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "sharvani@123",
+    database: "rssm_db",
+    port: 3307,
+  });
 
 // const db = mysql.createConnection({
 
