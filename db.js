5e0caf840bf4bd5486055c588d9548ee93ea4aa3
manju prasad <manjuprasad.4327@gmail.com>
2025-03-15 13:43:20 +0530

    Manju

--- db.js
+++ db.js
@@ -4,19 +4,19 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
 
-  //   host: "localhost",
-  //   user: "root",
-  //   password: "Manju@123",
-  //   database: "rssm_db",
-  //   port: 3306,
-  // });
-
-    host: 'localhost',
-    user: 'root',
-    password: 'ksk1005',
-    database: 'rssm_db',
+    host: "localhost",
+    user: "root",
+    password: "Manju@123",
+    database: "rssm_db",
     port: 3306,
-});
+  });
+
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'ksk1005',
+//     database: 'rssm_db',
+//     port: 3306,
+// });
 
 
 // const db = mysql.createConnection({
