baf4613f5fccd3c22cdc1047a5c75a9cda6d0569
unknown <sharvanikokkonda@gmail.com>
2025-03-17 18:31:14 +0530

    added status

--- db.js
+++ db.js
@@ -2,23 +2,6 @@ const mysql = require("mysql2");
 
 
 
-const db = mysql.createConnection({
-
-    host: "localhost",
-    user: "root",
-    password: "Manju@123",
-    database: "rssm_db",
-    port: 3306,
-  });
-
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'ksk1005',
-//     database: 'rssm_db',
-//     port: 3306,
-// });
-
-
 // const db = mysql.createConnection({
 
 //     host: "localhost",
@@ -37,13 +20,13 @@ const db = mysql.createConnection({
 
 
 
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "sharvani@123",
-//     database: "rssm_db",
-//     port: 3307,
-//   });
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "sharvani@123",
+    database: "rssm_db",
+    port: 3307,
+  });
 
 // const db = mysql.createConnection({
 
---
+++ routes/statusRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+router.put("/orders/update-status/:id", (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    const sql = "UPDATE orders SET status = ? WHERE id = ?";
+    
+    db.query(sql, [status, id], (err, result) => {
+      if (err) {
+        console.error("Error updating order status:", err);
+        res.status(500).json({ message: "Database error" });
+      } else {
+        res.json({ message: "Order status updated successfully" });
+      }
+    });
+  });
+
+module.exports = router; 
