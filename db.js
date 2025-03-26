c284e003afd2ce9a83c785348797b380fffce384
Saikrushna <kadarlasaikrushna01@gmail.com>
2025-03-26 18:32:24 +0530

    sai

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "ksk1005",
-//     database: "rssm_db",
-//     port: 3306,
-//   });
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "ksk1005",
+    database: "rssm_db",
+    port: 3306,
+  });
 
 // const db = mysql.createConnection({
 //     host: "localhost",
@@ -16,13 +16,13 @@ const mysql = require("mysql2");
 //     port: 3307,
 //   });
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Tharun@123",
-  database: "rssm_db",
-  port: 3307,
-});
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Tharun@123",
+//   database: "rssm_db",
+//   port: 3307,
+// });
 
 db.connect((err) => {
   if (err) {
