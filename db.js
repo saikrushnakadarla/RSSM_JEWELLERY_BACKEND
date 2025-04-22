332bd656317b421318139bb1ebafa45187a6b596
Tharun <tharunkumarreddy1212@gmail.com>
2025-04-22 21:00:02 +0530

    SubCategory

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql2");
 
-
 // const db = mysql.createConnection({
 //     host: "localhost",
 //     user: "root",
@@ -17,35 +16,30 @@ const mysql = require("mysql2");
 //   port: 3307,
 // });
 
-
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Tharun@123",
-//   database: "rssm_db",
-//   port: 3307,
-// });
-
-
-// const db = mysql.createConnection({
-
-//   host: "localhost",
-//   user: "root",
-//   password: "Tharun@123",
-//   database: "rssm_db",
-//   port: 3307,
-// });
-
-
-
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "ksk1005",
-    database: "rssm_db",
-    port: 3306,
-  });
+  host: "localhost",
+  user: "root",
+  password: "Tharun@123",
+  database: "rssm_db",
+  port: 3307,
+});
 
+// const db = mysql.createConnection({
+
+//   host: "localhost",
+//   user: "root",
+//   password: "Tharun@123",
+//   database: "rssm_db",
+//   port: 3307,
+// });
+
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "ksk1005",
+//     database: "rssm_db",
+//     port: 3306,
+//   });
 
 db.connect((err) => {
   if (err) {
